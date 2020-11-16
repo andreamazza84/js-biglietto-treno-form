19a59f0a94d18d61b78b2992410dfbbba2441080
andreamazza84 <andrea.mazza84@gmail.com>
2020-11-16 18:30:53 +0100

    Funzionante

--- assets/main.js
+++ assets/main.js
@@ -1,45 +1,45 @@
 var btnGenera = document.getElementById('button_genera');
 var btnAnnulla = document.getElementById('button_annulla');
 
+var isNumber = function isNumber(){
+  return typeof value === 'number' && isFinite();
+}
+
 btnGenera.addEventListener("click", function() {
   //genera Biglietto
-  console.log("Genera il biglietto");
 
   //selezionare valore input
   var fullNameInput = document.getElementById('full-name');
   var kmInput = document.getElementById('percorso');
   var fasciaInput = document.getElementById('fascia');
 
-  //La separazione consente di modificare altre proprietà
+
+  //La seguente separazione del valore consente di modificare altre proprietà
   var fullName = fullNameInput.value;
   var km = kmInput.value;
   var fascia = fasciaInput.value;
-  console.log(fullName, km, fascia);
 
   //calcolo biglietto treno
   var costoPerKm = 0.21;
   var ticketPrice = costoPerKm * km;
-  console.log(ticketPrice);
 
   // offerta
   var offer = 1;
   if (fascia == "minorenne"){
-    console.log("Applica uno scontro del 20%");
     offer = (1 - 0.2);
   }
   else if (fascia =="over65"){
-    console.log("Applica uno scontro del 40%");
-    offer = (1 - 0.4)
+    offer = (1 - 0.4);
   }
   // Applicazione dello sconto
   ticketPrice = ticketPrice * offer;
 
-  //Ora offer coincide con il valore dello sconto
+  //Ora "offer" coincide con il valore dello sconto
   offer = (1 - offer)*100;
 
   //Genera numero Carrozza
   var numberCarrozza = Math.ceil(Math.random() * 9) + 1;
-  var codiceCp = Math.floor(Math.random() * 9999) + 90000;
+  var codiceCp = Math.floor(Math.random() * 10000) + 90000;
 
   //Seleziono gli elementi dal documento
   var elName = document.getElementById('nome-passeggero');
@@ -54,8 +54,8 @@ btnGenera.addEventListener("click", function() {
   elSconto.innerHTML = "Sconto " + offer.toFixed(0) + "%";
   elCarrozza.innerHTML = numberCarrozza;
   elCodice.innerHTML = codiceCp;
-  elTicket.innerHTML = "€ " + ticketPrice;
-  document.getElementById('biglietto').style.display = "block";
+  elTicket.innerHTML = "€ " + ticketPrice.toFixed(2);
+  document.getElementById('biglietto').style.opacity = "1";
 });
 
 
@@ -64,9 +64,9 @@ btnAnnulla.addEventListener("click", function() {
   var fullNameInput = document.getElementById('full-name');
   var kmInput = document.getElementById('percorso');
   var fasciaInput = document.getElementById('fascia');
-  console.log("Annulla operazione");
-  document.getElementById('biglietto').style.display = "none";
+  document.getElementById('biglietto').style.opacity = "0";
 
   fullNameInput.value = "";
   kmInput.value = "";
+  fasciaInput.value = "minorenne";
 });
